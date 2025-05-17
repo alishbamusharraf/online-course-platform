f8c1520744710ecda50e276b759a00b7706039bc
Alishba Musharraf <162012518+alishbamusharraf@users.noreply.github.com>
2025-05-17 23:07:58 +0500

    Delete online-course-platform/online-course-platform directory

--- online-course-platform/online-course-platform/models/course.py
+++
@@ -1,7 +0,0 @@
-# models/course.py
-class Course:
-    def __init__(self, id, title, instructor, price):
-        self.id = id
-        self.title = title
-        self.instructor = instructor
-        self.price = price
--- online-course-platform/online-course-platform/utils/payment.py
+++
@@ -1,8 +0,0 @@
-# utils/payment.py
-class PaymentProcessor:
-    def __init__(self):
-        self.transactions = []
-
-    def process_payment(self, user_email, amount):
-        self.transactions.append({"user": user_email, "amount": amount})
-        return True
